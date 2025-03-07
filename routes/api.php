7e74f5a87e1b6554616b79fc0b14cfe8e0f3cc88
tishddd <tishdamas03@gmail.com>
2025-03-07 15:10:42 +0300

    APi's login and  dashbord 07.03.2025

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\DeviceEssueController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\OfficeController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -15,13 +16,17 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('jwt.auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'getUser']);
+    Route::get('/dashboard/stats', [DashboardController::class, 'stats']);
+    Route::get('/reports/transactions', [DashboardController::class, 'transactionsReport']);
+    Route::get('/reports/issues', [DashboardController::class, 'issuesReport']);
+    Route::get('/office-device-counts', [DashboardController::class, 'getOfficeDeviceCounts']);
+    Route::get('/total-device-counts', [DashboardController::class, 'getTotalDeviceCounts']);
 
     Route::apiResource('users', UserController::class);
     Route::apiResource('devices', DeviceController::class);
     Route::apiResource('devices_issues', DeviceEssueController::class);
     Route::apiResource('officies', OfficeController::class);
     Route::apiResource('transactions', TransactionController::class);
-    // Route::apiResource('dashbord', HomeController::class);
     // Route::apiResource('miscellaneous', MiscellaneousController::class);
   
 });
